b1b3fa5c7160ac1eb3043130066badb462ad10b0
AlicanCopur <alicancpr.33@gmail.com>
2022-01-08 10:55:06 +0300

    Implement read number1 and number2

--- simpleCalculatorInCS/src/Program.cs
+++ simpleCalculatorInCS/src/Program.cs
@@ -23,7 +23,7 @@ namespace simpleCalculatorInCS
             {
                 id = Int32.Parse(stringId);
             }
-            catch (Exception exception)
+            catch (Exception e)
             {
                 Console.WriteLine("Hatalı işlem numarası girildi.");
                 return;
@@ -32,7 +32,39 @@ namespace simpleCalculatorInCS
             Calculation calculation = Calculations.getById(id);
             if (calculation != null)
             {
-                Console.WriteLine("İşlem yapılacak.");
+                Console.WriteLine("Seçilen işlem: " + calculation.getPrettyName());
+                Console.WriteLine("Lütfen birinci sayıyı giriniz: ");
+                string number1Str = Console.ReadLine();
+                
+                double number1;
+
+                try
+                {
+                    number1 = Double.Parse(number1Str);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Girilen değer bir sayı olmalıdır.");
+                    return;
+                }
+
+                Console.WriteLine("Lütfen ikinci sayıyı giriniz: ");
+                string number2Str = Console.ReadLine();
+
+                double number2;
+                
+                try
+                {
+                    number2 = Double.Parse(number2Str);
+                }
+                catch (Exception e)
+                {
+                    Console.WriteLine("Girilen değer bir sayı olmalıdır.");
+                    return;
+                }
+                
+                Console.WriteLine("");
+                Console.WriteLine("İşlemin sonucu: " + calculation.calculate(number1, number2));
             }
             else
             {
