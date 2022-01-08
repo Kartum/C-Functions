2892425cfd18586941cea823f410d37e144491cd
AlicanCopur <alicancpr.33@gmail.com>
2022-01-08 09:16:55 +0300

    Create simple CS program

---
+++ simpleCalculatorInCS/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+namespace simpleCalculatorInCS
+{
+    class Program
+    {
+        public static void Main(string[] args)
+        {
+            Console.WriteLine("Merhaba! Lütfen yapacağınız işlemi seçin: ");
+            Console.WriteLine("- toplama");
+            Console.WriteLine("");
+            string islem = Console.ReadLine();
+            Console.WriteLine("Seçilen işlem: " + islem);
+        }
+    }
+}
